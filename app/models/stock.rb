1bce94e7177ef16db94d08e18422f52e7a670752
your name here <echen831@gmail.com>
2020-05-06 11:32:35 -0700

    added appropriate associations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,4 +5,8 @@ class Stock < ApplicationRecord
     belongs_to :exchange,
         foreign_key: :exchange_id,
         class_name: :Exchange
+
+    has_many :transactions,
+        foreign_key: :stock_id,
+        class_name: :Transaction
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
+
+    has_many :transactions,
+        foreign_key: :user_id,
+        class_name: :Transaction
     
     attr_reader :password
 
