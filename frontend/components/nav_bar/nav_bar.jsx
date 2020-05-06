5482e80e1ab8e43078e5b090123d247586f5f968
your name here <echen831@gmail.com>
2020-05-06 16:50:45 -0700

    updated flux and percent

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -6,15 +6,19 @@ class NavBar extends React.Component {
     render() {
         const {currentUser, logout} = this.props
         return (
-            <div class='top-nav'>
+            <div className='top-nav'>
                 <Link to='/stocks' className='header-logo'>
                     <h2>Robbyhood ➶</h2>
                 </Link>
                 <div className='nav-dropdown'>
                     <h2 className='nav-dropdown-btn'>Account</h2>
                     <div className='nav-dropdown-content'>
-                        <p>Hello, {currentUser.email}</p>
-                        <button onClick={logout} className='logout-btn'> Log Out</button>
+                        <p>{currentUser.email}</p>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <h2 onClick={logout} className='logout-btn'> Log Out</h2>
                     </div>
                 </div>
 
--- frontend/components/stock/hist_stock.jsx
+++ frontend/components/stock/hist_stock.jsx
@@ -6,6 +6,8 @@ class HistStock extends React.Component {
         super(props)
 
         this.filterData = this.filterData.bind(this)
+        this.setFlux = this.setFlux.bind(this)
+        this.setFluxPercent = this.setFluxPercent.bind(this)
     }
 
 
@@ -32,19 +34,53 @@ class HistStock extends React.Component {
         return res
     }
 
+    setFlux(num){
+        let res = ''
+        let arr = num.toString().split('.')
+        let int = arr[0];
+        let dec = arr[1];
+
+        if (int[0] !== '-') {
+            res += "+" + int + "."
+        } else {
+            res += int + '.'
+        }
+
+        if (!dec) {
+            res += '00'
+        } else if (dec.length === 1) {
+            res += '0' + dec
+        } else if (dec.length > 2) {
+            res += dec.slice(0, 2)
+        } else if (dec.length === 2) {
+            res += dec
+        }
+
+        return res
+    }
+
+    setFluxPercent(oldPrice, newPrice) {
+        let diff = oldPrice - newPrice
+        let per = diff/oldPrice
+        let res = per * 100
+        return this.setFlux(res) + '%'
+    }  
+
 
     render () {
+        const { range, name, histData } = this.props
         if (!this.props.histData) return null;
         // let data = this.props.histData
         let data;
-        this.props.range !== '1d' ? data = this.props.histData : data = this.filterData(this.props.histData)
+        range !== '1d' ? data = histData : data = this.filterData(histData)
         let close = data[data.length-1].high
         let open = data[0].high
         return (
             <div>
-                <h1>{this.props.name}</h1>
+                <h1>{name}</h1>
                 <p id='stockPrice'>${close}</p>
-                <p id='changePrice'>{close-open}</p>
+                <p id='changePrice'>{this.setFlux(close-open)}</p>
+                <p id='fluxPercent'>{this.setFluxPercent(close,open)}</p>
 
                 <LineChart data={data} 
                             width={500} 
@@ -55,45 +91,45 @@ class HistStock extends React.Component {
                             connectNulls 
                             dataKey="high" 
                             stroke="#8884d8" 
-                            dot={false} />
-                    <XAxis dataKey={this.props.range === "1d" ? 'label' : 'date'}
+                            dot={false} 
+                            />
+                    <XAxis dataKey={range === "1d" ? 'label' : 'date'}
                             tick={false} 
                             axisLine={false} 
-                            domain={['dataMin, dataMax'] } />
+                            domain={['dataMin, dataMax'] } 
+                            />
                     <YAxis domain={['dataMin, dataMax']} 
                             axisLine={false} 
-                            tick={false}/>
+                            tick={false}
+                            />
                     <Tooltip cursor={false}
                             position={{y: 0}} 
-                            content={<CustomTooltip oldPrice = {open} date = {this.props.range === '1d' ? data[0].date : null}/>}/>
+                            content={<CustomTooltip 
+                                        oldPrice = {open}
+                                        setFlux = {this.setFlux}
+                                        setFluxPercent = {this.setFluxPercent}
+                                        date = {range === '1d' ? data[0].date : null}/>}
+                            />
                 </LineChart>
             </div>
         )
     };
 };
 
-const setFlux = (num) => {
-    let res = ''
-    let arr = num.split('.')
-    let int = arr[0];
-    let dec = arr[1];
 
-    if (int[0] === '-') {
-        
-    }
-    return res
-}
 
 const CustomTooltip = (props) => {
     let oldPrice = props.oldPrice
     if (props.active) {
         const price = document.getElementById('stockPrice')
         const change = document.getElementById('changePrice')
+        const update = document.getElementById('fluxPercent')
         if (props.payload[0] && props.payload[0].payload) {
             let currPrice = (props.payload[0].payload.high)
             price.innerText = `$${currPrice}`
             let flux = currPrice - oldPrice
-            change.innerText = flux
+            change.innerText = props.setFlux(flux)
+            update.innerText = props.setFluxPercent(currPrice, oldPrice)
         }
         return (
             <div >
