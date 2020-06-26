af42f0225e2937319f2b95ab2afc2faa53264aa8
your name here <echen831@gmail.com>
2020-06-26 12:54:18 -0700

    add css transition and image to lmc on home page

--- frontend/components/home/lmc.jsx
+++ frontend/components/home/lmc.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 class LMC extends React.Component {
     constructor() {
@@ -32,7 +32,7 @@ class LMC extends React.Component {
                 <div className='lmc-content'>
                     <div className='lmc-actions'>
                         <ul>
-                            <div onClick={()=>this.upButton()}>Up</div>
+                            <div onClick={() => this.upButton()}>&#8593;</div>
                             <div
                                 className={this.state.index === 0 ? 'lmc-focus' : ''}
                                 onClick={()=>this.setState({index: 0})}>Learn</div>
@@ -42,13 +42,15 @@ class LMC extends React.Component {
                             <div
                                 className={this.state.index === 2 ? 'lmc-focus' : ''}
                                 onClick={() => this.setState({ index: 2 })}>Customize</div>
-                            <div onClick={()=>this.downButton()}>Down</div>
+                            <div onClick={() => this.downButton()}>&#8595;</div>
                         </ul>
                     </div>
                     <div className='lmc-details'>
                         <div id={this.state.index === 0 ? 'lmc-show' : 'lmc-hide'}
                              className='lmc-slide'>
-                            <div className='lmc-image'>Image1</div>
+                            <div className='lmc-image'>
+                                <img src="/assets/lmc_img_1.png" alt=""/>
+                            </div>
                             <div className='lmc-msg'>
                                 <h2>Learn As You Grow</h2>
                                 <p>Our goal is to make investing in financial markets more affordable, more intuitive, and more fun, no matter how much experience you have (or don’t have).</p>
@@ -56,7 +58,9 @@ class LMC extends React.Component {
                         </div>
                         <div id={this.state.index === 1 ? 'lmc-show' : 'lmc-hide'}
                              className='lmc-slide'>
-                            <div className='lmc-image'>Image2</div>
+                            <div className='lmc-image'>
+                                <img src="/assets/lmc_img_2.png" alt=""/>
+                            </div>
                             <div className='lmc-msg'>
                                 <h2>Manage Your Portfolio</h2>
                                 <p>Keep your portfolio in your pocket. Everything you need to manage your assets is available in a single app.</p>
@@ -65,7 +69,9 @@ class LMC extends React.Component {
                         </div>
                         <div id={this.state.index === 2 ? 'lmc-show' : 'lmc-hide'}
                              className='lmc-slide'>   
-                            <div className='lmc-image'>Image3</div>
+                            <div className='lmc-image'>
+                                <img src='/assets/lmc_img_3.png' alt=""/>
+                            </div>
                             <div className='lmc-msg'>
                                 <h2>Keep Tabs on Your Money</h2>
                                 <p>Set up customized news and notifications to stay on top of your assets as casually or as relentlessly as you like. Controlling the flow of info is up to you.</p>
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,15 @@ module.exports = {
                         presets: ['@babel/env', '@babel/react']
                     }
                 },
-            }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
         ]
     },
     devtool: 'source-map'
