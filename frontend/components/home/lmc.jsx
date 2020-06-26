550303280f7ea9862b6075cfb9bdb75d1008b81e
your name here <echen831@gmail.com>
2020-06-26 10:42:09 -0700

    add tabs functionality to lmc

--- frontend/components/home/lmc.jsx
+++ frontend/components/home/lmc.jsx
@@ -8,6 +8,22 @@ class LMC extends React.Component {
             index: 0
         }
     }
+
+    upButton() {
+        if (this.state.index === 0) {
+            this.setState({index: 2})
+        } else {
+            this.setState({index: this.state.index - 1})
+        }
+    }
+
+    downButton() {
+        if (this.state.index === 2) {
+            this.setState({ index: 0 })
+        } else {
+            this.setState({ index: this.state.index + 1 })
+        }  
+    }
     
     render() {
         return (
@@ -16,23 +32,29 @@ class LMC extends React.Component {
                 <div className='lmc-content'>
                     <div className='lmc-actions'>
                         <ul>
-                            <div>Up</div>
-                            <div>Learn</div>
-                            <div>Manage</div>
-                            <div>Customize</div>
-                            <div>Down</div>
+                            <div onClick={()=>this.upButton()}>Up</div>
+                            <div
+                                className={this.state.index === 0 ? 'lmc-focus' : ''}
+                                onClick={()=>this.setState({index: 0})}>Learn</div>
+                            <div
+                                className={this.state.index === 1 ? 'lmc-focus' : ''}
+                                onClick={() => this.setState({ index: 1 })}>Manage</div>
+                            <div
+                                className={this.state.index === 2 ? 'lmc-focus' : ''}
+                                onClick={() => this.setState({ index: 2 })}>Customize</div>
+                            <div onClick={()=>this.downButton()}>Down</div>
                         </ul>
                     </div>
-                    <div className='lmc-image'>
-                        <div>
+                    <div className='lmc-details'>
+                        <div className={this.state.index === 0 ? 'lmc-show' : 'lmc-hide'}>
                             <div>Image1</div>
                             <div>Content1</div>
                         </div>
-                        <div>
+                        <div className={this.state.index === 1 ? 'lmc-show' : 'lmc-hide'}>
                             <div>Image2</div>
                             <div>Content2</div>
                         </div>
-                        <div>   
+                        <div className={this.state.index === 2 ? 'lmc-show' : 'lmc-hide'}>   
                             <div>Image3</div>
                             <div>Content3</div>
                         </div>
