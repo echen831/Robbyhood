2e40b3c9086f224822b211a2b1690a851b22d715
your name here <echen831@gmail.com>
2020-06-28 21:53:13 -0700

    add product modal

---
+++ frontend/components/home/product_modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class ProductModal extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ProductModal
