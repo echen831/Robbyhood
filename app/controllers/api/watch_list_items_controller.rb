cf4991d352870a45be435950115a8325f4508a04
your name here <echen831@gmail.com>
2020-07-10 23:15:42 -0700

    update watch list item controller

--- app/controllers/api/watch_list_items_controller.rb
+++ app/controllers/api/watch_list_items_controller.rb
@@ -2,11 +2,13 @@ class Api::WatchListItemsController < ApplicationController
     
     def create
         @watch_list_item = WatchListItem.new
-        
+
 
     end
 
     def destroy
+        @watch_list_item = WatchListItem.find(params[:id])
+        @watch_list_item.destroy
     end
 
 
