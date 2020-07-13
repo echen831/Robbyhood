22e5297a15fbff0def3ba2761b372b4980e00aed
your name here <echen831@gmail.com>
2020-07-12 20:38:27 -0700

    add graph reducer

---
+++ frontend/reducers/graphs_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_ONE_STOCK, RECEIVE_STOCKS } from '../actions/stock_actions';
+
+const graphsReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+
+    switch (action.type) {
+
+        case RECEIVE_ONE_STOCK:
+            return Object.assign({}, oldState, { [action.symbol]: action.stock })
+
+        default:
+            return oldState;
+    }
+};
+
+export default graphsReducer
