0ccd9c0cbaf30c9866a46456418197c78835b6a9
your name here <echen831@gmail.com>
2020-07-13 16:36:21 -0700

    add errors to create transaction

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -10,31 +10,26 @@ class Api::TransactionsController < ApplicationController
         @stock = Stock.find_by(symbol: params[:transaction][:symbol])
         @transaction.stock_id = @stock.id
         @transaction.user_id = current_user.id
+
+        
+        transaction_amount = @transaction.price * @transaction.num_shares
+        buying_power = current_user.calculate_buying_power
+        shares_owned = current_user.shares_owned(@transaction.stock_id)
+
+
+        if transaction_amount > buying_power && @transaction.transactions_type == 'buy'
+            render json: ['Not enough buying power'], status: 401
+        elsif @transaction.num_shares <= 0 
+            render json: ['Shares must be greater than 0'], status: 422
+        elsif @transaction.num_shares > shares_owned && @transaction.transactions_type == 'sell'
+            render json: ['Not enough shares'], status: 401
+        else 
             if @transaction.save 
                 render json: ['Success'], status: 200
             else 
                 render json: @transaction.errors.full_messages, status: 422
             end
-
-        
-        # transaction_amount = @transaction.price * @transaction.num_shares
-        # buying_power = current_user.calculate_buying_power
-        # shares_owned = current_user.shares_owned(@transaction.stock_id)
-
-
-        # if transaction_amount > buying_power && @transaction.transactions_type == 'buy'
-        #     render json: ['Not enough buying power'], status: 401
-        # elsif @transaction.num_shares <= 0 
-        #     render json: ['Shares must be greater than 0'], status: 422
-        # elsif @transaction.num_shares > shares_owned && transaction.transactions_type == 'sell'
-        #     render json: ['Not enough shares'], status: 401
-        # else 
-        #     if @transaction.save 
-        #         render json: ['Success'], status: 200
-        #     else 
-        #         render json: @transaction.errors.full_messages, status: 422
-        #     end
-        # end
+        end
     end
 
     def update
