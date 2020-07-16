82e1b21d51daae85f048fbe94864b1b8f96a8131
your name here <echen831@gmail.com>
2020-07-16 15:38:42 -0700

    add flux percentage to portfolio item

--- frontend/components/stock/portfolio_item.jsx
+++ frontend/components/stock/portfolio_item.jsx
@@ -11,6 +11,8 @@ class PortfolioItem extends React.Component {
         fetchOneDayStock(symbol, '1d')
     }
 
+
+
     findCurrentPrice(stocks, symbol) {
         let price = null
         if (!stocks || !stocks[symbol]) return null
@@ -29,12 +31,61 @@ class PortfolioItem extends React.Component {
         return price
     }
 
+    setPrice(data) {
+        if (data) {
+
+            let arr = data.toString().split('.')
+            let res = arr[1]
+
+            if (!res) {
+                res = '00'
+            } else if (res.length == 1) {
+                res += 0
+            } else if (res.length > 2) {
+                res = res.slice(0, 2)
+            }
+
+            return arr[0] + '.' + res
+        }
+    }
+
+    setFlux(num) {
+        let res = ''
+        let arr = num.toString().split('.')
+        let int = arr[0];
+        let dec = arr[1];
+
+        int[0] !== '-' ? res += "+" + int + "." : res += int + '.'
+
+
+        if (!dec) {
+            res += '00'
+        } else if (dec.length === 1) {
+            res += '0' + dec
+        } else if (dec.length === 2) {
+            res += dec
+        } else if (dec.length > 2) {
+            res += dec.slice(0, 2)
+        }
+
+        return res
+    }
+
+    setFluxPercent(oldPrice, newPrice) {
+        let diff = oldPrice - newPrice
+        let per = diff / oldPrice
+        let res = per * 100
+        return this.setFlux(res) + '%'
+    } 
+
+
     render () {
 
         let { currentUser, symbol, idx, myStocks } = this.props
-        if (!myStocks) return null
+        if (!myStocks || !myStocks[symbol]) return null
         let currPrice = this.findCurrentPrice(myStocks, symbol)
         let purchasePrice = currentUser.stock_prices[symbol]
+        let openPrice = myStocks[symbol][0].high
         return (
             <div className='portfolio-item-container'
                  key={idx}>
@@ -46,8 +97,8 @@ class PortfolioItem extends React.Component {
                         {currentUser.stocks_owned[symbol] > 1 ? 'Shares' : 'Share'}</p>
                 </div>
                 <div className='pi-stock-info'>
-                    <p>${currPrice}</p>
-                    <p>{purchasePrice}</p>
+                    <p>${this.setPrice(currPrice)}</p>
+                    <p>{this.setFluxPercent(openPrice, currPrice)}</p>
                 </div>
             </div>
         )
