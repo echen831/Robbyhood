5ee3aa04e9d624bb4076d4fc680fc13d1f6c09ec
your name here <echen831@gmail.com>
2020-07-15 18:57:07 -0700

    add css to buy sell stock component

--- frontend/components/stock/buy_sell_stock.jsx
+++ frontend/components/stock/buy_sell_stock.jsx
@@ -46,6 +46,7 @@ class BuySellStock extends React.Component {
 
     render () {
         let { stock, stocks, name, symbol, currentUser, makeTransaction } = this.props
+        let { num_shares, transactions_type } = this.state
         let transaction = {
             num_shares: this.state.num_shares,
             price: this.showAmount(stock[stock.length - 1].high),
@@ -64,28 +65,40 @@ class BuySellStock extends React.Component {
                 <div className='stock-bar'>
                     <div className='buy-sell-container'>
                         <p  id='buy-tab'
+                            className={transactions_type === 'buy' ? 'bs-selected' : ''}
                             onClick={() => this.setState({transactions_type: 'buy'})}
                             >Buy {symbol.toUpperCase()}</p>
                         <p  id={ ownShares === '0' ? 'display-none' : 'sell-tab'}
+                            className={transactions_type === 'sell' ? 'bs-selected' : ''}
                             onClick={() => this.setState({transactions_type: 'sell'})}
                             >Sell {symbol.toUpperCase()}</p>
                     </div>
-                    <div>
-                        <div>Shares <input type="number" onChange={(e)=> this.setState({num_shares: e.currentTarget.value})}/>
-                        
+                    <div className='buy-sell-info-container'>
+                        <div className='shares-container'>
+                            <p>Shares</p>
+                            <input type="number"
+                                   placeholder='0' 
+                                   onChange={(e)=> this.setState({num_shares: e.currentTarget.value})}/>
                         </div>
-                        <div>Market Price  {this.showAmount(stock[stock.length - 1].high)}</div>
-                        <div> 
-                            {this.state.transactions_type === 'buy' ? 'Estimated Cost: ' : 'Estimated Credit: '}
-                            {this.showAmount(stock[stock.length - 1].high  * this.state.num_shares)}
+                        <div className='mp-container'>
+                            <p>Market Price</p> 
+                            <p>${this.showAmount(stock[stock.length - 1].high)}</p></div>
+                        <div className='cost-credit-container'> 
+                            <p>{this.state.transactions_type === 'buy' ? 'Estimated Cost: ' : 'Estimated Credit: '}</p>
+                            <p>${this.showAmount(stock[stock.length - 1].high  * this.state.num_shares)}</p>
                         </div>
-                        <button onClick={()=> makeTransaction(transaction)}>{this.state.transactions_type}</button>
-                        <p>{this.state.transactions_made}</p>
+                        <div className='review-order-container'>
+                            <button>Review Order</button>
+                        </div>
                     </div>
 
                     <div>
-                        <div>Buying Power Available: {this.showAmount(currentUser.buying_power)}</div>
-                        <div>{`Owned Shares: ${ownShares}`}</div>
+                        <div id={this.state.transactions_type === 'sell' ? 'display-none' : ''}>Buying Power Available: {this.showAmount(currentUser.buying_power)}</div>
+                        <div id={this.state.transactions_type === 'buy' ? 'display-none' : ''}>{`Owned Shares: ${ownShares}`}</div>
+                    </div>
+
+                    <div>
+                        <button onClick={() => makeTransaction(transaction)}>{this.state.transactions_type}</button>
                     </div>
             
                 </div>
