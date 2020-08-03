122622ed46ec4a23c744a5a4cef383fbe08a0f32
your name here <echen831@gmail.com>
2020-08-02 17:17:48 -0700

    resize charts

--- frontend/components/stock/hist_stock.jsx
+++ frontend/components/stock/hist_stock.jsx
@@ -156,7 +156,7 @@ class HistStock extends React.Component {
                 </div>
 
                 <LineChart data={data}
-                    width={500}
+                    width={625}
                     height={300}
                     className='chart'
                 // margin={{ top: 10, right: 0, bottom: 0, left: 0 }}
--- frontend/components/stock/portfolio.jsx
+++ frontend/components/stock/portfolio.jsx
@@ -208,7 +208,7 @@ class Portfolio extends React.Component {
                     </div>
                 </div>
                 <LineChart data={data}
-                    width={500}
+                    width={625}
                     height={300}
                     className='chart'
                 // margin={{ top: 10, right: 0, bottom: 0, left: 0 }}
