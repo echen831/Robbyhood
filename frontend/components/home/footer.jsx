f66b846bb47dbe218cf9da6cd761dab71653bc62
your name here <echen831@gmail.com>
2020-08-11 17:15:14 -0700

    change footer icons

--- frontend/components/home/footer.jsx
+++ frontend/components/home/footer.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
+
+    const gitHub = <FontAwesomeIcon icon={['fab', 'github']} size='2x' />
+    const linkedIn = <FontAwesomeIcon icon={['fab', 'linkedin']} size='2x' />
+    const angelList = <FontAwesomeIcon icon={['fab', 'angellist']} size='2x' />
 
     return (
         <div className='footer-body'>
             <div id='about-me'>About Me: </div>
             <div className='footer-contacts'>
-                <a id='footer-github'
-                   target='_blank'
-                   href="https://github.com/echen831">
-                   </a>
-                <a id='footer-linkedin'
-                   target='_blank'
-                   href="https://www.linkedin.com/in/eric-chen-782b951a9/" > 
-                    </a>
-                <a id='footer-facebook'
+                <a id='footer-icon'
                     target='_blank'
-                    href="https://www.facebook.com/profile.php?id=11708937" >
+                    href="https://github.com/echen831">{gitHub}
+                </a>
+                <a id='footer-icon'
+                    target='_blank'
+                    href="https://www.linkedin.com/in/eric-chen-782b951a9/" >{linkedIn}
+                </a>
+                <a id='footer-icon'
+                    target='_blank'
+                    href="https://angel.co/u/eric-chen-80" >{angelList}
                 </a>
             </div>
         </div>
