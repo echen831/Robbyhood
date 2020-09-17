f0febcb86a0f3c077ff17e7eeba48bb44a15e3ae
your name here <echen831@gmail.com>
2020-09-16 17:46:12 -0700

    remove unused imports

--- frontend/components/home/footer.jsx
+++ frontend/components/home/footer.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
 
